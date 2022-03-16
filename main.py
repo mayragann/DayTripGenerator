b4ea1c37426ed175b6e1f034b3fed6b20a7f6365
Mayra <mayragann@gmail.com>
2022-03-16 09:45:08 -0400

    added final confirmation of user choices

--- main.py
+++ main.py
@@ -22,51 +22,89 @@ import random
 
 
 
+
+
+ 
 def location_choice(list_of_locations):
 
-    user_answer = 'y'
     
+    user_answer = 'y'
+   
+    
+
     while list_of_locations != user_answer:
         trip_location = random.choice(random_locations)
         list_of_locations = input(f'Does {trip_location} sound like a good place to visit? y/n: ')
-        if list_of_locations != user_answer:
-            print("Not a problem")
-        else:
-            print('Excellent Choice')
-
-location_choice(random_locations)
         
+        if(list_of_locations == user_answer):
+            print('Excellent Choice')
+            return trip_location
+
+        print("Not a problem")
+        return location_choice(random_locations)
+           
+    
+        
+            
+   
+           
+        
+        
+
+chosen_location = location_choice(random_locations)
+
+
 # (5 points): As a user, I want a restaurant to be randomly selected for my day trip. 
 
-def food_choice(food_locations):
+def food_choice(food):
 
-    user_answer = 'y'
     
-    while food_locations != user_answer:
-        restaurant= random.choice(random_food)
-        food_locations = input(f'Does {restaurant} sound like a good place eat at? y/n: ')
-        if food_locations != user_answer:
-            print("Let's try something different")
-        else:
-            print('Sounds Delicious!!')
+    user_answer = 'y'
+   
+    
 
-food_choice(random_food)
+    while food != user_answer:
+        food_approved = random.choice(random_food)
+        food = input(f'Does {food_approved} sound like a good place to eat at? y/n: ')
+        
+        if(food == user_answer):
+            print('Excellent Choice')
+            return food_approved
+
+        print("Not a problem")
+        return food_choice(random_food)
+           
+    
+        
+            
+
+approved_food = food_choice(random_food)
 
 # (5 points): As a user, I want a mode of transportation to be randomly selected for my day trip. 
 
 def transportation_choice(transportation):
 
-    user_answer = 'y'
     
-    while transportation != user_answer:
-        mode_of_transport= random.choice(random_transportation)
-        transportation = input(f'Does {mode_of_transport} sound like a good way to get around? y/n: ')
-        if transportation != user_answer:
-            print("Let's try again")
-        else:
-            print('Sounds fun!!')
+    user_answer = 'y'
+   
+    
 
-transportation_choice(random_transportation)
+    while transportation != user_answer:
+        transport_approved = random.choice(random_transportation)
+        transportation = input(f'Does {transport_approved} sound like a good way to go around? y/n: ')
+        
+        if(transportation == user_answer):
+            print('Excellent Choice')
+            return transport_approved
+
+        print("Not a problem")
+        return transportation_choice(random_transportation)
+           
+    
+        
+            
+
+approved_transport = transportation_choice(random_transportation)
 
 
 
@@ -74,22 +112,43 @@ transportation_choice(random_transportation)
 
 def entertainment_choice(entertainment):
 
-    user_answer = 'y'
     
-    while entertainment != user_answer:
-        mode_of_entertainment= random.choice(random_entertainment)
-        entertainment = input(f'Does {mode_of_entertainment} sound like a good way to get around? y/n: ')
-        if entertainment != user_answer:
-            print("Let's try again")
-        else:
-            print('Sounds fun!!')
+    user_answer = 'y'
+   
+    
 
-entertainment_choice(random_entertainment)
+    while entertainment != user_answer:
+        entertainment_approved = random.choice(random_entertainment)
+        entertainment = input(f'Does {entertainment_approved} sound like a good way to go do? y/n: ')
+        
+        if(entertainment == user_answer):
+            print('Excellent Choice')
+            return entertainment_approved
+
+        print("Not a problem")
+        return entertainment_choice(random_entertainment)
+           
+    
+        
+            
+
+approved_transport = entertainment_choice(random_entertainment)
 
 
 # (15 points): As a user, I want to be able to randomly re-select a destination, restaurant, mode of transportation, and/or form of entertainment if I don’t like one or more of those things. 
 
+def destination_confirmation(location):
+    # chosen_location = location_choice(random_locations)
+    location = input(f'Does your original choice of {chosen_location} still your main choice? y/n: ')
+ 
+    if location != "y":
+        print('Next choice')
+        chosen_location = location_choice(random_locations)
+    else:
+        print('Perfect')
+    return 
 
+destination_confirmation(location_choice)
 
 
 # (10 points): As a user, I want to be able to confirm that my day trip is “complete” once I like all of the random selections. 
@@ -98,7 +157,11 @@ entertainment_choice(random_entertainment)
 
 
 
+
 # (10 points): As a user, I want to display my completed trip in the console. 
+
+
+
 # (5 points): As a developer, I want all of my functions to have a Single Responsibility. Remember, each function should do just one thing!
 
 
